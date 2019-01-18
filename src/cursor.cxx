54ab45c34dea21603c79bb8aa5b855372eb597e4
Leonid Yuriev <leo@yuriev.ru>
2019-01-18 03:50:21 +0300

    fpta: fix minor typos.

--- src/cursor.cxx
+++ src/cursor.cxx
@@ -593,7 +593,7 @@ int fpta_cursor_locate(fpta_cursor *cursor, bool exactly, const fpta_value *key,
       if (!mdbx_seek_data) {
         /* Поиск без уточнения по дубликатам. Если индекс допускает
          * дубликаты, то следует перейти к последнему, что будет
-         * сделао ниже. */
+         * сделано ниже. */
         break;
       }
 
@@ -1123,7 +1123,7 @@ int fpta_apply_visitor(
 
 //----------------------------------------------------------------------------
 
-FPTA_API int fpta_cursor_info(fpta_cursor *cursor, fpta_cursor_stat *stat) {
+int fpta_cursor_info(fpta_cursor *cursor, fpta_cursor_stat *stat) {
   int rc = fpta_cursor_validate(cursor, fpta_read);
   if (unlikely(rc != FPTA_SUCCESS))
     return rc;
